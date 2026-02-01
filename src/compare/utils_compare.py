2891616109dddd7d584fef8754989f344b46c2d8
= <=>
2026-02-01 12:28:33 -0500

    add logs

--- src/compare/utils_compare.py
+++ src/compare/utils_compare.py
@@ -1,4 +1,21 @@
 
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input_model_datastore", dest="input_model_datastore", type=str)
+    parser.add_argument("--input_feats_datastore", dest="input_feats_datastore", type=str)
+    parser.add_argument("--output_predict_datastore", dest="output_predict_datastore", type=str)
+    parser.add_argument("--feats_version", dest="feats_version", type=str)
+    parser.add_argument("--n_eval_periodos", dest="n_eval_periodos", type=int, default=-1)
+    parser.add_argument("--model_periodo", dest="model_periodo", type=int)
+    parser.add_argument("--model_version", dest="model_version", type=str)
+    parser.add_argument("--periodo", dest="periodo", type=int, default=-1)
+    parser.add_argument("--mode", dest="mode", type=str)
+    parser.add_argument("--with_tipo", dest="with_tipo", type=str)
+    args = parser.parse_args()
+    return args
 
 def get_mapping_tipos(periodo: int) -> dict:
     """
@@ -14,7 +31,9 @@ def get_mapping_tipos(periodo: int) -> dict:
             'inicial_estacional':True, 
             'continuidad_estacional':False, 
             'inicial_regular':True, 
-            'continuidad_regular':True}
+            'continuidad_regular':True,
+            'continuidad_regular_horario':True
+            }
         
         return tipos
     elif periodo%100 == 2:
@@ -23,6 +42,7 @@ def get_mapping_tipos(periodo: int) -> dict:
             'continuidad_estacional':True,
             'inicial_regular':True, 
             'continuidad_regular':True, 
+            'continuidad_regular_horario':True
             }
         return tipos
     else:
@@ -30,5 +50,8 @@ def get_mapping_tipos(periodo: int) -> dict:
             'inicial_estacional':False, 
             'continuidad_estacional':False, 
             'inicial_regular':True, 
-            'continuidad_regular':True}
+            'continuidad_regular':True,
+            'continuidad_regular_horario':True
+            }
         return tipos
+    
